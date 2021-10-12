745e8d8415e8c3a051c1aeaddc7d9ce08c0a5f8c
Terry Tumen <44890849+terry10k@users.noreply.github.com>
2021-10-12 14:29:11 -0500

    multiple piping
    Piping works now, except when used with sort. All that is needed now is to add id to myshell.c

--- test.c
+++ test.c
@@ -5,36 +5,93 @@
 
 extern char **getlineShell();
 void pipeIndex(char **args, int pipeNum, int pipeCollection[]);
+void args_split(char **args, int start, int end, char **buf);
+
 
 main() {
   int i;
   char **args; 
+  char **buf;
 
   while(1) {
 	args = getlineShell();
 	
 	for(i = 0; args[i] != NULL; i++) {
 		printf("Argument %d: %s\n", i, args[i]);
-    	}
+    }
+
 	int fd[10][2];
 
-
-
-//	execvp(args[0], args);
-/*
 	int pipeNum = pipeNumCounter(args);
+	
+	printf("\npipenum is %d\n\n", pipeNum);
 
-	printf("pipenum is %d\n",pipeNum);
+	int pipeCollection[pipeNum+2];
+	pipeCollection[0] = -1;
 
-	int pipeCollection[pipeNum];
-
+	// this changes pipecollection buffer
 	pipeIndex(args, pipeNum, pipeCollection);
-	int i;
-	printf("pipenum in function is %d\n", pipeNum);
-	for(i=0;pipeCollection[i]!=NULL;i++) {
-		printf("pc index %d is %d\n", i, pipeCollection[i]);
+
+	int p;
+		
+	if (pipeNum > 0) {	
+	// if there is one pipe there would need to be 2 forked processes
+	for( i = 0; i < pipeNum+1; i++) {
+		
+		// create first pipe
+		if (pipe(fd[i]) == -1) {
+			printf("error creating pipe");
+			return 1;
+		}
+		
+		// get args for loop iteration
+		args_split(args, pipeCollection[i], pipeCollection[i+1], &buf[0]);
+		
+		
+		
+			
+		if (fork()==0) {
+			//child
+			
+			printf("\niteration %d\n", i);
+			
+			printf("pipecol index %d and %d is %d and %d\n",i, i+1, pipeCollection[i], pipeCollection[i+1]);
+
+		
+			for (p=0; buf[p]!=NULL; p++) {
+				printf("buf index %d is %s\n", p, buf[p]);
+			}
+			
+			if(i!=pipeNum) {
+				dup2(fd[i][1], 1);
+				close(fd[i][0]);
+				close(fd[i][1]);
+			}
+			
+			if (i!=0) {
+				dup2(fd[i-1][0], STDIN_FILENO);
+				close(fd[i-1][0]);
+				close(fd[i-1][1]);
+			}
+			
+			execvp(buf[0],buf);
+			
+		}
+		
+		//parent
+		if(i!=0) {
+			close(fd[i-1][0]);
+			close(fd[i-1][1]);
+		}
+		
+		wait(NULL);
 	}
-*/
+	
+
+	} else {
+		execvp(args[0],args);
+	}
+
   }
 }
 
@@ -58,15 +115,18 @@ int maxSize(char **args) {
 }
 */
 
-//need indices of pipes first
-char** arrSlice(char **args, int start, int end) {
-	int sliceSize = end - start - 1;
-
-//	char** sliced[sliceSize][];
-
-	
-	
-
+//this splits the string array into parts based on pipes
+//needs a fencepost for the first and last slice
+void args_split(char **args, int start, int end, char **buf)
+{
+    int i;
+    int j = 0;
+    for (i = start+1; i < end; i++) {
+	//	printf("lalala %s\n", args[i]);
+        buf[j] = args[i];
+		j++;
+    }
+    buf[j] = NULL;
 }
 
 int pipeNumCounter(char **args) {
@@ -80,17 +140,21 @@ int pipeNumCounter(char **args) {
 }
 
 // modifies pipenumc array with indices the indexes of the pipes
-void pipeIndex(char **args, int pipeNum, int pipeCollection[]) {
+void pipeIndex(char **args, int pipeNum, int *pipeCollection) {
 	
 	int k;
-	int j = 0; // indexer for pipeCollection
+	int j = 1; // indexer for pipeCollection starts at 1 cuz index 0 is -1
 	for(k = 0; args[k] != NULL; k++) {
-		printf("%d", k);
 		if(args[k][0] == '|') {
-			(pipeCollection)[j] = k;
+			pipeCollection[j] = k;
 			j++;
+			
+			//printf("j is %d\n", j);
 		}
 	}
+	
+	
+	pipeCollection[j] = k;
 	
 }
 
